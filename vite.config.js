52575ad4a152d82b901000a5afad9eb83d0259a6
Umidjon <sokcho69@workershigh.com>
2025-01-12 21:52:01 +0900

    vite configjs

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true, // Enable source maps in the build
-  },
+ 
 })
